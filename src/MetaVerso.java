b9a16aa8aef01be10222af174c45c4ba26de2757
Talita Castro Freitas <talitacastrofreitas@gmail.com>
2022-08-04 20:47:13 -0300

    Controle

---
+++ src/MetaVerso.java
@@ -0,0 +1,11 @@
+public class MetaVerso {
+    public static void main(String[] args) {
+        int Indice= 13, Soma = 0, K=0;
+        if (K<Indice);
+        {
+            K= K+1;
+            Soma = Soma+K;
+        }
+        System.out.println(Soma);
+    }
+}
